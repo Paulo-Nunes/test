23a8fb6d0b47a57728c5d5a095cac763dde3639c
Paulo-Nunes <pauloamnunes@hotmail.com>
2016-10-20 18:39:41 -0400

    Try pinMode and analogWrite

--- GrovePiExtra/src/edu/ccsu/cs417/groveextra/GrovePiExtra.java
+++ GrovePiExtra/src/edu/ccsu/cs417/groveextra/GrovePiExtra.java
@@ -5,9 +5,11 @@
  */
 package edu.ccsu.cs417.groveextra;
 
-import com.dexterind.grovepi.Grovepi;
 import com.dexterind.grovepi.sensors.Buzzer;
 import com.pi4j.io.i2c.I2CFactory;
+import static com.pi4j.wiringpi.Gpio.PWM_OUTPUT;
+import static com.pi4j.wiringpi.Gpio.analogWrite;
+import static com.pi4j.wiringpi.Gpio.pinMode;
 import java.io.IOException;
 
 /**
@@ -24,17 +26,24 @@ public class GrovePiExtra {
 
     public void runDemo() throws IOException, InterruptedException, I2CFactory.UnsupportedBusNumberException {
         Buzzer buzz = new Buzzer(boardNumber);
-        buzz.turnOn();
+        buzz.setVolume(50);
         Thread.sleep(1000);
         buzz.turnOff();
         
-        buzz.setFrequency(1);
-        buzz.turnOn();
+        pinMode(3, PWM_OUTPUT);
+        analogWrite(3, 50);
         Thread.sleep(1000);
         
-        buzz.setFrequency(100);
-        buzz.turnOn();
-        Thread.sleep(1000);
+        
+        //buzz.setFrequency(1);
+        //buzz.setVolume(50);
+        //Thread.sleep(1000);
+        
+        //grovepi.analogWrite(3, 50);
+        
+        //buzz.setFrequency(100);
+        //buzz.turnOn();
+        //Thread.sleep(1000);
         
         //for (float brightness = 0; brightness <= 100; brightness += 10) {
         //    buzz.setVolume(brightness);
