7cc355f7d603f1a42f44982c9438384ed582294b
Paulo-Nunes <pauloamnunes@hotmail.com>
2016-10-21 11:27:13 -0400

    T

--- GrovePiExtra/src/edu/ccsu/cs417/groveextra/GrovePiExtra.java
+++ GrovePiExtra/src/edu/ccsu/cs417/groveextra/GrovePiExtra.java
@@ -12,7 +12,7 @@ import static com.pi4j.wiringpi.Gpio.millis;
 import static com.pi4j.wiringpi.Gpio.pwmSetClock;
 import static com.pi4j.wiringpi.Gpio.pwmSetRange;
 import static com.pi4j.wiringpi.Gpio.pwmWrite;
-import static com.pi4j.wiringpi.Gpio.wiringPiSetupGpio;
+import static com.pi4j.wiringpi.Gpio.wiringPiSetupPhys;
 import static com.pi4j.wiringpi.SoftPwm.softPwmCreate;
 import static com.pi4j.wiringpi.SoftPwm.softPwmWrite;
 import java.io.IOException;
@@ -50,8 +50,8 @@ public class GrovePiExtra {
 
 
             //System.out.println("wiringPiSetUp():  "+wiringPiSetup());
-            System.out.println("wiringPiSetUp():  "+wiringPiSetupGpio());
-            //wiringPiSetupPhys();
+            //System.out.println("wiringPiSetUp():  "+wiringPiSetupGpio());
+            System.out.println("wiringPiSetUp():  "+wiringPiSetupPhys());
             //wiringPiSetupSys();
             Thread.sleep(1000);
             buzz.turnOn();
