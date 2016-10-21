48d610d098029119eb9269be0c0cd531a4d5aabb
Paulo-Nunes <pauloamnunes@hotmail.com>
2016-10-21 12:01:07 -0400

    T

--- GrovePiExtra/src/edu/ccsu/cs417/groveextra/GrovePiExtra.java
+++ GrovePiExtra/src/edu/ccsu/cs417/groveextra/GrovePiExtra.java
@@ -5,17 +5,13 @@
  */
 package edu.ccsu.cs417.groveextra;
 
-import com.dexterind.grovepi.sensors.Buzzer;
-import com.dexterind.grovepi.sensors.Led;
-import com.pi4j.io.i2c.I2CFactory;
-import static com.pi4j.wiringpi.Gpio.millis;
-import static com.pi4j.wiringpi.Gpio.pwmSetClock;
-import static com.pi4j.wiringpi.Gpio.pwmSetRange;
-import static com.pi4j.wiringpi.Gpio.pwmWrite;
-import static com.pi4j.wiringpi.Gpio.wiringPiSetupSys;
-import static com.pi4j.wiringpi.SoftPwm.softPwmCreate;
-import static com.pi4j.wiringpi.SoftPwm.softPwmWrite;
-import java.io.IOException;
+import static com.pi4j.wiringpi.Gpio.HIGH;
+import static com.pi4j.wiringpi.Gpio.LOW;
+import static com.pi4j.wiringpi.Gpio.OUTPUT;
+import static com.pi4j.wiringpi.Gpio.delay;
+import static com.pi4j.wiringpi.Gpio.delayMicroseconds;
+import static com.pi4j.wiringpi.Gpio.digitalWrite;
+import static com.pi4j.wiringpi.Gpio.pinMode;
 
 /**
  *
@@ -23,125 +19,59 @@ import java.io.IOException;
  */
 public class GrovePiExtra {
 
-    private final int boardNumber;
+int speakerPin = 3;
 
-    public GrovePiExtra(int boardNumber) {
-        this.boardNumber = boardNumber;
+int length = 15; // the number of notes
+char notes[] = {'c','c','g','g','a','a','g','f','f','e','e','d','d','c',' '}; // a space represents a rest
+int beats[] = { 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 4 };
+int tempo = 300;
+
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(speakerPin, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(speakerPin, LOW);
+    delayMicroseconds(tone);
+  }
+}
+
+void playNote(char note, int duration) {
+  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+  
+  // play the tone corresponding to the note name
+  for (int i = 0; i < 8; i++) {
+    if (names[i] == note) {
+      playTone(tones[i], duration);
     }
+  }
+}
 
-    public void runDemo() throws IOException, InterruptedException, I2CFactory.UnsupportedBusNumberException {
-        Buzzer buzz = new Buzzer(boardNumber);
-        Led led = new Led(14);
-        Led led2 = new Led(6);
+void setup() 
+{
+  pinMode(speakerPin, OUTPUT);
+}
 
-//        buzz.setVolume(10);
-//        Thread.sleep(1000);
-//        buzz.setVolume(100);
-//        buzz.turnOff();
-        
-//        buzz.setFrequency(1);
-//        buzz.turnOn();
-//        Thread.sleep(1000);
-//        
-//        buzz.setFrequency(50);
-
-//        led.turnOn();
-//        Thread.sleep(1000);
-
-
-            //System.out.println("wiringPiSetUp():  "+wiringPiSetup());
-            //System.out.println("wiringPiSetUp():  "+wiringPiSetupGpio());
-            //System.out.println("wiringPiSetUp():  "+wiringPiSetupPhys());
-            System.out.println("wiringPiSetUp():  "+wiringPiSetupSys());
-            Thread.sleep(100);
-            //buzz.turnOn();
-            //Thread.sleep(1000);
-            //buzz.turnOff();
-            
-            System.out.println("softPwmCreate():  "+softPwmCreate(3, 40, 100));
-            Thread.sleep(100);
-            
-            //buzz.turnOn();
-            //Thread.sleep(1000);
-            softPwmWrite(3, 50);
-            System.out.println("softPwmWrite()");
-            Thread.sleep(100);
-            //buzz.turnOff();
-            //Thread.sleep(1000);
-            
-            buzz.turnOn();
-            Thread.sleep(500);
-            pwmSetRange (500);
-            System.out.println(millis ());
-            pwmSetClock(40);
-            Thread.sleep(1500);
-             System.out.println("pwmwmWrite(3, 50)");
-            pwmWrite(3, 50);
-            Thread.sleep(2000);
-            System.out.println("pwmwmWrite(3, 255)");
-            pwmWrite(3, 255);
-            Thread.sleep(2000);
-            buzz.turnOff();
-            
-
-//        for(int i = 0; i<255; i+=2){
-//            analogWrite(14,5,4,4);
-//            Thread.sleep(100);
-//
-//        }
-//        led.turnOn();
-//        Thread.sleep(1000);
-//          
-//          analogWrite(3,1);
-//          Thread.sleep(1000);
-//          
-//          digitalWrite(5,1);
-//          Thread.sleep(1000);
-//          
-//          analogWrite(5,1);
-//          Thread.sleep(1000);
-//          
-//          digitalWrite(6,1);
-//          Thread.sleep(1000);
-//          
-//          analogWrite(6,1);
-//          Thread.sleep(1000);
-
-          //buzz.turnOn();
-        //Thread.sleep(1000);
-    //led.turnOff();
-          
-//        buzz.setFrequency(100);
-//        buzz.turnOn();
-//        Thread.sleep(1000);
-        
-//        for (float brightness = 0; brightness <= 100; brightness += 2) {
-//            led.setBrightness(brightness);
-//            Thread.sleep(100);
-//        }
-        
-//        for (int i = 0; i <= 20; i++) {
-//            digitalWrite(i,1);
-//            Thread.sleep(100);
-//
-//            analogWrite(i,1);
-//            Thread.sleep(100);
-//        }
-        buzz.turnOff();
-        led.turnOff();
-        led2.turnOff();
+void loop() {
+  for (int i = 0; i < length; i++) {
+    if (notes[i] == ' ') 
+{
+      delay(beats[i] * tempo); // rest
+    } 
+else 
+{
+      playNote(notes[i], beats[i] * tempo);
     }
+    
+    // pause between notes
+    delay(tempo / 2); 
+  }
+}
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         try {
-            // Default is D3
-            //Grovepi grovePi = new Grovepi();
-            int boardNumber = 3;
-            if (args.length == 1) {
-                boardNumber = Integer.parseInt(args[0]);
-            }
-            GrovePiExtra ledDemo = new GrovePiExtra(boardNumber);
-            ledDemo.runDemo();
+            setup();
+            loop();
         } catch (Exception e) {
             e.printStackTrace();
         }
