a1ae290fd7f772973ccf036e3c1e8342be3b9ad3
Paulo-Nunes <pauloamnunes@hotmail.com>
2016-10-20 21:40:39 -0400

    T

--- GrovePiExtra/src/edu/ccsu/cs417/groveextra/GrovePiExtra.java
+++ GrovePiExtra/src/edu/ccsu/cs417/groveextra/GrovePiExtra.java
@@ -1,92 +1,101 @@
-// START SNIPPET: control-gpio-snippet
-
 /*
- * #%L
- * **********************************************************************
- * ORGANIZATION  :  Pi4J
- * PROJECT       :  Pi4J :: Java Examples
- * FILENAME      :  ControlGpioExample.java
- *
- * This file is part of the Pi4J project. More information about
- * this project can be found here:  http://www.pi4j.com/
- * **********************************************************************
- * %%
- * Copyright (C) 2012 - 2016 Pi4J
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Lesser Public License for more details.
- *
- * You should have received a copy of the GNU General Lesser Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-3.0.html>.
- * #L%
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
+package edu.ccsu.cs417.groveextra;
 
-import com.pi4j.io.gpio.GpioController;
-import com.pi4j.io.gpio.GpioFactory;
-import com.pi4j.io.gpio.GpioPinDigitalOutput;
-import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;
+import com.dexterind.grovepi.sensors.Buzzer;
+import com.dexterind.grovepi.sensors.Led;
+import com.pi4j.io.i2c.I2CFactory;
+import static com.pi4j.wiringpi.Gpio.analogWrite;
+import static com.pi4j.wiringpi.Gpio.digitalWrite;
+import java.io.IOException;
 
 /**
- * This example code demonstrates how to perform simple state
- * control of a GPIO pin on the Raspberry Pi.
  *
- * @author Robert Savage
+ * @author Paulo
  */
 public class GrovePiExtra {
 
-    public static void main(String[] args) throws InterruptedException {
+    private final int boardNumber;
 
-        System.out.println("<--Pi4J--> GPIO Control Example ... started.");
-
-        // create gpio controller
-        final GpioController gpio = GpioFactory.getInstance();
-
-        // provision gpio pin #01 as an output pin and turn on
-        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
-
-        // set shutdown state for this pin
-        pin.setShutdownOptions(true, PinState.LOW);
-
-        System.out.println("--> GPIO state should be: ON");
-
-        Thread.sleep(5000);
-
-        // turn off gpio pin #01
-        pin.low();
-        System.out.println("--> GPIO state should be: OFF");
-
-        Thread.sleep(5000);
-
-        // toggle the current state of gpio pin #01 (should turn on)
-        pin.toggle();
-        System.out.println("--> GPIO state should be: ON");
-
-        Thread.sleep(5000);
-
-        // toggle the current state of gpio pin #01  (should turn off)
-        pin.toggle();
-        System.out.println("--> GPIO state should be: OFF");
-
-        Thread.sleep(5000);
-
-        // turn on gpio pin #01 for 1 second and then off
-        System.out.println("--> GPIO state should be: ON for only 1 second");
-        pin.pulse(1000, true); // set second argument to 'true' use a blocking call
-
-        // stop all GPIO activity/threads by shutting down the GPIO controller
-        // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
-        gpio.shutdown();
-
-        System.out.println("Exiting ControlGpioExample");
+    public GrovePiExtra(int boardNumber) {
+        this.boardNumber = boardNumber;
     }
+
+    public void runDemo() throws IOException, InterruptedException, I2CFactory.UnsupportedBusNumberException {
+        Buzzer buzz = new Buzzer(boardNumber);
+        Led led = new Led(5);
+        Led led2 = new Led(6);
+
+//        buzz.setVolume(10);
+//        Thread.sleep(1000);
+//        buzz.setVolume(100);
+//        buzz.turnOff();
+        
+//        buzz.setFrequency(1);
+//        buzz.turnOn();
+//        Thread.sleep(1000);
+//        
+//        buzz.setFrequency(50);
+        //buzz.turnOn();
+//          digitalWrite(3,1);
+//          Thread.sleep(1000);
+//          
+//          analogWrite(3,1);
+//          Thread.sleep(1000);
+//          
+//          digitalWrite(5,1);
+//          Thread.sleep(1000);
+//          
+//          analogWrite(5,1);
+//          Thread.sleep(1000);
+//          
+//          digitalWrite(6,1);
+//          Thread.sleep(1000);
+//          
+//          analogWrite(6,1);
+//          Thread.sleep(1000);
+          
+
+          
+    
+          
+//        buzz.setFrequency(100);
+//        buzz.turnOn();
+//        Thread.sleep(1000);
+        
+//        for (float brightness = 0; brightness <= 100; brightness += 2) {
+//            led.setBrightness(brightness);
+//            Thread.sleep(100);
+//        }
+        
+        for (int i = 0; i <= 20; i++) {
+            digitalWrite(i,1);
+            Thread.sleep(500);
+
+            analogWrite(i,1);
+            Thread.sleep(500);
+        }
+        buzz.turnOff();
+        led.turnOff();
+        led2.turnOff();
+    }
+
+    public static void main(String[] args) {
+        try {
+            // Default is D3
+            //Grovepi grovePi = new Grovepi();
+            int boardNumber = 3;
+            if (args.length == 1) {
+                boardNumber = Integer.parseInt(args[0]);
+            }
+            GrovePiExtra ledDemo = new GrovePiExtra(boardNumber);
+            ledDemo.runDemo();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
 }
-//END SNIPPET: control-gpio-snippet
