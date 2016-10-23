b68726a2279a31fe69aec766f18a0c7dcd96b79f
Paulo-Nunes <pauloamnunes@hotmail.com>
2016-10-23 12:52:33 -0400

    T

--- GrovePiExtra/src/edu/ccsu/cs417/groveextra/GrovePiExtra.java
+++ GrovePiExtra/src/edu/ccsu/cs417/groveextra/GrovePiExtra.java
@@ -61,7 +61,27 @@ public class GrovePiExtra {
 //////////        
 //////////        thing.buzz(99, 2000);
 
+pinMode(0, OUTPUT);
+pinMode(1, OUTPUT);
+pinMode(2, OUTPUT);
 pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);
+pinMode(6, OUTPUT);
+pinMode(7, OUTPUT);
+pinMode(8, OUTPUT);
+pinMode(9, OUTPUT);
+pinMode(10, OUTPUT);
+pinMode(11, OUTPUT);
+pinMode(12, OUTPUT);
+pinMode(13, OUTPUT);
+pinMode(14, OUTPUT);
+pinMode(15, OUTPUT);
+pinMode(16, OUTPUT);
+pinMode(17, OUTPUT);
+pinMode(18, OUTPUT);
+
+
 analogWrite(0, 255);
 analogWrite(1, 255);
 analogWrite(2, 255);
